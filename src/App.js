287fdb5cdec5d94dc647679793089d303e489db6
mrsamia <15203060@iubat.edu>
2022-01-29 21:38:01 +0600

    SRM | Functional_cmpnt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Functional_cmpnt from './components/Functional_cmpnt';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>React</h1>
+      <h1>React</h1>
+      <Functional_cmpnt />
     </div>
   );
 }
---
+++ src/components/Functional_cmpnt.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Functional_cmpnt() {
+    return (
+        <div>
+            <h1>Im from functional component</h1>
+        </div>
+    );
+}
+export default Functional_cmpnt;

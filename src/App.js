25f2332ea4e9a69c68aebcca201709c3b27f0a01
mrsamia <15203060@iubat.edu>
2022-01-29 22:10:34 +0600

    SRM | passing_arguments_to_event_handler

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
     <div className="App">
       <h1>React</h1>
       <Functional_cmpnt name="samia" country="Bangla"/>
-      <Functional_cmpnt name="sami" country="English" />
-      <Functional_cmpnt  name="sam"country="Math" />
-      <Functional_cmpnt  name="sa" country="Bangla"/>
+      
     </div>
   );
 }
--- src/components/Functional_cmpnt.js
+++ src/components/Functional_cmpnt.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function Functional_cmpnt(props) {
+
+function Dothis(a){
+    alert(a);
+}
+
+
+
     return (
         <div>
             <h1>Hello I'm {props.name} and i'm from {props.country}</h1>
+
+            <button onClick={Dothis.bind(this,"Button is clicked")}>Click me</button>
         </div>
     );
 }
